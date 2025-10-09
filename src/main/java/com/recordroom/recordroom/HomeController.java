5a538b77ab7367af384930c8db7fd7d500c17e48
nicerodetn <nicerodetn@gmail.com>
2025-10-09 21:00:41 +0530

    Basci Login Update

--- src/main/java/com/recordroom/recordroom/HomeController.java
+++ src/main/java/com/recordroom/recordroom/HomeController.java
@@ -1,10 +1,18 @@
 package com.recordroom.recordroom;
 
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -12,15 +20,42 @@ public class HomeController {
 
 
     @GetMapping("/homepage")
-    public String homepage()
-    {
+    public String homepage() {
         return "layout/outsidelayout";
     }
 
     @GetMapping("/dashboard")
-    public String dashboard()
-    {
+    public String dashboard() {
         return "layout/insidelayout";
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(
+            @RequestParam String username,
+            @RequestParam String password,
+            HttpSession session,
+            Model model) throws ServletException {
+
+        if (username.equalsIgnoreCase("admin") && password.equalsIgnoreCase("admin")) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("HX-Redirect", "/record/dashboard"); // 👈 destination page
+            return new ResponseEntity<>(headers, HttpStatus.OK);
+        } else {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("HX-Redirect", "/record/homepage"); // 👈 destination page
+            return new ResponseEntity<>(headers, HttpStatus.OK);
+        }
+    }
+
+
+    @GetMapping("/logout")
+    public ResponseEntity<Void> logout(
+            HttpSession session,
+            Model model) throws ServletException {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("HX-Redirect", "/record/homepage"); // 👈 destination page
+            return new ResponseEntity<>(headers, HttpStatus.OK);
+
     }
 
 
